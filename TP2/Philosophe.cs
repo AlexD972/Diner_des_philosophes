393b9096c8e9463155edfacb8dcf57299ed5ac2a
AlexD972 <alexandreduclos83@gmail.com>
2024-05-15 00:08:03 +0200

    Correction du bug lors de la coloration des assiettes

--- TP2/Philosophe.cs
+++ TP2/Philosophe.cs
@@ -19,7 +19,7 @@ namespace TP2
         Graphics g;
         Form1 F;
         int i;
-        public Philosophe(Panel panel1, Semaphore fourchette_g, Semaphore fourchette_d, Graphics g, int i, TextBox box, Form1 F)
+        public Philosophe(Semaphore fourchette_g, Semaphore fourchette_d, Graphics g, int i, TextBox box, Form1 F)
         {
             this.fourchette_d = fourchette_d;
             this.fourchette_g = fourchette_g;
@@ -39,11 +39,14 @@ namespace TP2
                 box.Text = "Penser";
                 Thread.Sleep(1000);
                 fourchette_d.WaitOne();
-				//Assiettes
-				g.FillEllipse(Brushes.Red, (float)(F.R * Math.Cos(2 * i * Math.PI / 5) + F.X0 - F.X0 / (F.tailleA * 2)), (float)(F.R * Math.Sin(2 * i * Math.PI / 5) + F.Y0 - F.Y0 / (2 * F.tailleA)), F.X0 / F.tailleA, F.Y0 / F.tailleA);
-				//Baguettes
-				g.FillEllipse(Brushes.Red, (float)(F.R * Math.Cos(2 * i * Math.PI / 5) + F.X0 - F.X0 / (F.tailleBH * 2)), (float)(F.R * Math.Sin(2 * i * Math.PI / 5) + F.Y0 - F.Y0 / (2 * F.tailleBH)), F.X0 / (F.tailleBE), F.Y0 / (3 * F.tailleBE));
-				fourchette_g.WaitOne();
+                lock (g)
+                {
+                    //Assiettes
+                    g.FillEllipse(Brushes.Red, (float)(F.R * Math.Cos(2 * i * Math.PI / 5) + F.X0 - F.X0 / (F.tailleA * 2)), (float)(F.R * Math.Sin(2 * i * Math.PI / 5) + F.Y0 - F.Y0 / (2 * F.tailleA)), F.X0 / F.tailleA, F.Y0 / F.tailleA);
+                    //Baguettes
+                    g.FillEllipse(Brushes.Red, (float)(F.R * Math.Cos(2 * i * Math.PI / 5) + F.X0 - F.X0 / (F.tailleBH * 2)), (float)(F.R * Math.Sin(2 * i * Math.PI / 5) + F.Y0 - F.Y0 / (2 * F.tailleBH)), F.X0 / (F.tailleBE), F.Y0 / (3 * F.tailleBE));
+                }
+                fourchette_g.WaitOne();
                 //Manger
                 box.Text = "Manger";
                 Thread.Sleep(1000);
